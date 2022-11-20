840b0043ee589c711925e6c826d24eab036ba55d
lokesh-mali <mali.lokesh5050@gmail.com>
2022-11-20 21:57:13 +0530

    form+render

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import '../src/App.css'
 function App() {
+  //make a toogle for show name
+
+  const [gallery, setGallery] = useState([]);
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+    const [name, url] = e.target;
+    const newData = {
+      name: name.value,
+      url: url.value,
+    };
+    setGallery([...gallery, newData]);
+    e.target[0].value = ""
+    e.target[1].value = ""
+  };
+
+  var lo = <h4>Loading...</h4>
+  if (gallery.length > 0) {
+    lo = gallery.map((e, i) => (
+      <li key={i}>
+        Name: {e.name} , Image : <img width={"30%"} height={"50%"} src={e.url} alt="Invalid Url" />
+      </li>
+    ));
+  }
+  console.log(gallery);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={handleFormSubmit}>
+        <input type="text" placeholder="Type the Author Name" />
+        <input type="text" placeholder="Url" />
+        <input type="submit" value="Submit" />
+      </form>
+      <ul>{lo}</ul>
     </div>
   );
 }
