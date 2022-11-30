343ff01c01b0d130f1a6f8600799f44c66f76b35
lokesh-mali <mali.lokesh5050@gmail.com>
2022-11-30 22:32:27 +0530

    deleting cards

--- src/App.js
+++ src/App.js
@@ -61,12 +61,22 @@ function App() {
     showData = gallery.map((e, i) => (
       <div className="card" key={i} style={{ width: "18rem" }}>
         <img src={e.url} className="card-img-top me-3 mb-3" alt="..." />
-        <div className="card-body">
+        <div className="card-body d-flex justify-content-between align-items-center">
+          <span>
           <h5 className="card-title">{e.book}</h5>
           <p className="card-text">{e.desc}</p>
+          </span>
+          <i onClick={()=> handleDelete(i)} className="ri-delete-bin-line"></i>
         </div>
       </div>
     ));
+  }
+
+
+  const handleDelete = (index) =>{
+    const newGallery = gallery.filter((e,i)=> index !== i);
+    console.log(newGallery)
+    setGallery(newGallery)
   }
 
   return (
